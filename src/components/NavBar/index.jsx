f93f701d712194c7d7c5a2a0a5a0d82adb3ea22d
Ali Rehman <145115863+AliRehman12@users.noreply.github.com>
2025-01-06 20:02:02 +0500

    Update index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -80,7 +80,7 @@ export default function NavBar() {
                 <img
                   className="h-12 w-auto"
                   src={images.brandLogoDark}
-                  alt="Go Back to Home"
+                  alt="Home"
                   height={12}
                   width={245}
                 />
@@ -88,7 +88,7 @@ export default function NavBar() {
                 <img
                   className="h-12 w-auto"
                   src={images.brandLogoLight}
-                   alt="Go Back to Home"
+                   alt="Home"
                   height={12}
                   width={245}
                 />
