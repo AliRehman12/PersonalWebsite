142a72ffeed75681bd800de3a49ab1d55db40188
Ali Rehman <alirehman12@example.com>
2025-07-01 10:40:20 +0500

    Initial commit with local files

---
+++ src/constants/image.js
@@ -0,0 +1,57 @@
+import upworkIcon from "../assets/upwork.svg";
+import emailIcon from "../assets/email.svg";
+import githubIcon from "../assets/github.svg";
+import instagramicon from "../assets/instagram.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+import htmlLogo from "../assets/logo/html-logo.svg";
+import cssLogo from "../assets/logo/css-logo.svg";
+import javascriptLogo from "../assets/logo/javascript-logo.svg";
+import reactLogo from "../assets/logo/react-logo.svg";
+import tailwindLogo from "../assets/logo/tailwind-logo.svg";
+import gitLogo from "../assets/logo/git-logo.svg";
+import githubLogo from "../assets/logo/github-logo.svg";
+import nextLogo from "../assets/logo/next-logo.svg";
+import nodejsLogo from "../assets/logo/nodejs-logo.svg";
+import mongodbLogo from "../assets/logo/mongodb-logo.svg";
+import postgresqlLogo from "../assets/logo/postgresql-logo.svg";
+import hubspotLogo from "../assets/logo/hubspot-logo.svg";
+import personalportfoliodashboard from "../assets/projects/personalportfoliodashboard.png";
+import EmpHer from "../assets/projects/EmpHer.png";
+import felixImage from "../assets/my_pic.png";
+import catifyImage from "../assets/projects/catify-visible.png";
+import agencyImage from "../assets/projects/agency-visible.png";
+import pixiedustImage from "../assets/projects/pixiedust.png";
+import slothImage from "../assets/projects/languagesloth-visible.png";
+import errorIllustration from "../assets/illustrations/404-vector.svg";
+import successIllustration from "../assets/illustrations/success-vector.svg";
+
+
+export default {
+  EmpHer,
+  personalportfoliodashboard,
+  pixiedustImage,
+  emailIcon,
+  postgresqlLogo,
+  mongodbLogo,
+  nodejsLogo,
+  upworkIcon,
+  githubIcon,
+  instagramicon,
+  linkedinIcon,
+  htmlLogo,
+  cssLogo,
+  javascriptLogo,
+  reactLogo,
+  tailwindLogo,
+  gitLogo,
+  githubLogo,
+  nextLogo,
+  hubspotLogo,
+  felixImage,
+  catifyImage,
+  agencyImage,
+  slothImage,
+  errorIllustration,
+  successIllustration,
+  
+};
